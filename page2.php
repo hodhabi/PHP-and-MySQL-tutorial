0dbe0bb55b365d74d8b5cc0ab33a01b50f96be6d
DrHamadODHABI <hodhabi@gmail.com>
2016-09-15 16:47:27 +0400

    Add upload file feature
    Added include to the new upload.php file that handles file upload.
    The file upload.php uses the function uploadFiles that receives three 3
    parameters that are name, tmp_name,  and size to upload the file.
    This parameters come from  a single form input of type-“file”.   Only
    when the function uploadFile returns true, the changes will be added to
    the database.

--- page2.php
+++ page2.php
@@ -6,12 +6,12 @@
 <HEAD>
 </HEAD>
 <BODY>
-<FORM action="update.php" method="post">
+<FORM action="update.php" method="post" enctype="multipart/form-data">
 
 <input name="id" type="text" value ="<?php echo $_GET['id']; ?>">
 <br><input name="firstname" type="text" value ="<?php echo $_GET['fname']; ?>">
 <br><input name="lastname" type="text" value ="<?php echo $_GET['lname']; ?>">
-
+<br>    <input type="file" name="fileToUpload" id="fileToUpload">
 <br><input name = "submit" type="submit" value="submit">
 </form>
 
--- update.php
+++ update.php
@@ -6,12 +6,16 @@
 
 
 include("dbconnect.php");
+include("upload.php");
 
+
+if(uploadFile($_FILES["fileToUpload"]["name"],$_FILES["fileToUpload"]["tmp_name"],$_FILES["fileToUpload"]["size"])==true)
+{
 $sql = "UPDATE MyGuests SET firstname = '" . $_POST['firstname'] . 
-"',lastname = '" . $_POST['lastname'] . "' WHERE id = " . $_POST['id'];
+"',lastname = '" . $_POST['lastname'] . "', filename='" . $_FILES["fileToUpload"]["name"] . "' WHERE id = " . $_POST['id'];
 
 $result = dbcon("CIA2503",$sql);
 
 header('Location: index.php'); 
-
+}
 ?>
